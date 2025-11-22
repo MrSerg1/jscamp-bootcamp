377284a3f5e98b9ff1f9874de5af7884c674a3c6
MrSerg1 <sergiotejon96@gmail.com>
2025-11-21 20:07:12 -0500

    Solución ejercicio 2

--- 02-ejercicio-javascript/apply-button.js
+++ 02-ejercicio-javascript/apply-button.js
@@ -1,1 +1,14 @@
 /* Aquí va la lógica para dar funcionalidad al botón de "Aplicar" */
+document.addEventListener("DOMContentLoaded", () => {
+  const jobResultsSection = document.querySelector(".jobs-listings"); // Selecciona la sección que vamos a escuchar.
+
+  jobResultsSection.addEventListener("click", (event) => {
+    const element = event.target;
+    if (element.classList.contains("button-apply-job")) {
+      element.textContent = "¡Aplicado!"; // Cambia el texto del botón.
+      element.disabled = true; // Deshabilita el botón para evitar múltiples clics
+      element.style.backgroundColor = "#4caf50"; // Cambia el color de fondo a verde
+    } 
+  });
+});
+ 
