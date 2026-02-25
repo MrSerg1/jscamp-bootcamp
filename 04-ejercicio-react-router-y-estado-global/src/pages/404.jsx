8764beee4d5038f050e09b1b1cc3324bf5d30449
madeval <alvarez.fing@gmail.com>
2026-02-25 17:16:10 -0300

    Change NotFoundPage export to default export for lazy loading compatibility

--- 04-ejercicio-react-router-y-estado-global/src/pages/404.jsx
+++ 04-ejercicio-react-router-y-estado-global/src/pages/404.jsx
@@ -1,4 +1,5 @@
-export function NotFoundPage () {
+// Exportamos la función como default, para poder importarla en App.jsx y usarla con lazy load
+export default function NotFoundPage () {
   return (
     <main>
       <h1>404 - Página no encontrada</h1>
