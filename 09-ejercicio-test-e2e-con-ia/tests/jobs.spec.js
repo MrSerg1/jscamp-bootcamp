2798aae1809ebb3f41d2cabea2b941881f70608b
sTejon <devstejon@gmail.com>
2026-04-11 15:32:02 -0500

    chore: update code structure for improved readability and maintainability

--- 09-ejercicio-test-e2e-con-ia/tests/jobs.spec.js
+++ 09-ejercicio-test-e2e-con-ia/tests/jobs.spec.js
@@ -5,3 +5,15 @@ test('Verifies that there is a search input field on the page', async ({ page })
     const searchInput = await page.getByRole('searchbox')
     await expect(searchInput).toBeVisible()
 })
+
+test('verifies that the user can type in the search input field', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+    const searchInput = await page.getByRole('searchbox')
+    await searchInput.fill('React')
+    await page.getByRole('button', { name: 'Buscar' }).click()
+    const jobCards = await page.locator('.jobs-listings')
+    await expect(jobCards).toBeVisible()
+    
+    const jobTitles = await jobCards.first().locator('h3').first()
+    await expect(jobTitles).toHaveText('Desarrollador de Software Senior')
+})
