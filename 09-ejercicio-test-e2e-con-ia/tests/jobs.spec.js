d0cf6f38eb970be34326fe3cb4cf9f72c14ce867
sTejon <devstejon@gmail.com>
2026-04-11 14:55:56 -0500

    Refactor code structure for improved readability and maintainability

--- 09-ejercicio-test-e2e-con-ia/tests/jobs.spec.js
+++ 09-ejercicio-test-e2e-con-ia/tests/jobs.spec.js
@@ -1,4 +1,7 @@
-/* Aquí irá el código de tu test */
+import { test, expect } from '@playwright/test'
 
-// @ts-check
-// import { test, expect } from '@playwright/test'
+test('Verifies that there is a search input field on the page', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+    const searchInput = await page.getByRole('searchbox')
+    await expect(searchInput).toBeVisible()
+})
