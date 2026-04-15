143f8859b2c0339764883207cbd0aa011d9949d9
sTejon <devstejon@gmail.com>
2026-04-14 20:04:18 -0500

    first excercise completed primitive types

--- 10-ejercicio-typescript/types.ts
+++ 10-ejercicio-typescript/types.ts
@@ -1,8 +1,6 @@
 /* Aquí deberás definir los tipos literales del primer ejercicio */
 
-/* 
-export type ExperienceLevel = 
-export type WorkMode = 
-export type ApplicationStatus = 
-export type Technology = 
-*/
+export type ExperienceLevel =  'Junior' | 'mid' | 'Senior'| 'Lead';
+export type WorkMode = 'remoto' | 'presencial' | 'híbrido';
+export type ApplicationStatus = 'pending' | 'reviewing' | 'accepted' | 'rejected';
+export type Technology = 'javascript' | 'typescript' | 'react' | 'node' | 'python' | 'java' | 'flutter' | 'android' | 'ios' | 'swift' | 'kotlin' | 'dart'| 'go' | 'rust' | 'php' | 'ruby' | 'c#';
