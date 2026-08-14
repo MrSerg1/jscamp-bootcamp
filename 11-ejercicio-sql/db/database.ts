fe67f0ee8d674e54a2c52f860259714a8d53afb4
sTejon <devstejon@gmail.com>
2026-08-14 15:43:45 -0500

    Configure sqlite db connection using better-sqlite3

--- 11-ejercicio-sql/db/database.ts
+++ 11-ejercicio-sql/db/database.ts
@@ -1,1 +1,8 @@
-/* Aquí irá tu código del primer ejercicio */
+import Database from "better-sqlite3";
+
+const db = new Database("jobs.db");
+
+db.pragma("journal_mode = WAL"); // Mejora la concurrencia y performance
+db.pragma("foreign_keys = ON"); // sqlite por defecto ignora las claves foráneas, esto las habilita.
+
+export {db as export};
