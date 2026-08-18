84f86c34dee21f33e1675314d9e62ba376712859
sTejon <devstejon@gmail.com>
2026-08-17 20:03:42 -0500

    Implement getAll in JobModel with filters

--- 11-ejercicio-sql/dudas.md
+++ 11-ejercicio-sql/dudas.md
@@ -1,1 +1,4 @@
 <!-- Aquí puedes poner tus dudas sobre el ejercicio -->
+**1 duda**: En @models/job.ts linea 10 almacene la respuesta de la base de datos en una lista con un any porque es un almacenamiento temporal ya que luego reorganizo los datos siguiendo las indicaciones de types.ts. ¿está bien dejar esa lista con un any o debería crear una interface jobRaw en la que pueda almacenar los datos del trabajo aunque sea una lista de uso temporal? ¿vale la pena la inversión de tiempo y lineas para eso o esta bien dejarlo como any?
+
+**2 duda**: ¿Dónde se debería hacer la paginación en la API o usando SQL en la base de datos?
--- 11-ejercicio-sql/models/job.ts
+++ 11-ejercicio-sql/models/job.ts
@@ -1,18 +1,74 @@
-import crypto from 'node:crypto'
-import type { Job, CreateJobDTO, UpdateJobDTO, JobFilters } from '../types'
+import crypto from "node:crypto";
+import type {
+  Job,
+  CreateJobDTO,
+  UpdateJobDTO,
+  JobFilters,
+  JobContent,
+} from "../types";
+import { export as db } from "../db/database.js";
 
 export class JobModel {
   // Obtener todos los jobs con filtros opcionales
   static async getAll(filters?: JobFilters): Promise<Job[]> {
     // TODO: Debemos hacer la consulta a la base de datos para obtener todos los resultados, y por cada filtro,
     // debemos agregarlo a la consulta
-    return []
+    let query = "SELECT * FROM jobs";
+    const conditions = [];
+    const values = [];
+
+    if (filters?.modality) {
+      conditions.push("modality = ?");
+      values.push(filters.modality);
+    }
+
+    if (filters?.level) {
+      conditions.push("level = ?");
+      values.push(filters.level);
+    }
+
+    if (filters?.tech) {
+      conditions.push(
+        "id IN (SELECT job_id FROM job_technologies WHERE technology = ?)",
+      );
+      values.push(filters.tech);
+    }
+
+    if (conditions.length > 0) {
+      query += " WHERE " + conditions.join(" AND ");
+    }
+
+    const jobData = db.prepare(query).all(...values) as any[];
+
+    return jobData.map((partialJob) => ({
+      id: partialJob.id,
+      title: partialJob.title,
+      company: partialJob.company,
+      location: partialJob.location,
+      description: partialJob.description,
+      data: {
+        technology: (
+          db
+            .prepare(
+              "SELECT technology FROM job_technologies WHERE job_id = ?",
+            )
+            .all(partialJob.id) as { technology: string }[]
+        ).map((tech) => tech.technology),
+        modality: partialJob.modality,
+        level: partialJob.level,
+      },
+      content: db
+        .prepare(
+          "SELECT description, responsibilities, requirements, about  FROM job_content WHERE job_id = ?",
+        )
+        .get(partialJob.id) as JobContent | undefined,
+    }));
   }
 
   // Obtener un job por ID
   static async getById(id: string): Promise<Job | undefined> {
     // TODO: Debemos hacer la consulta a la base de datos para obtener el job por ID
-    return undefined
+    return undefined;
   }
 
   // Crear un nuevo job
@@ -20,21 +76,21 @@ export class JobModel {
     const newJob: Job = {
       id: crypto.randomUUID(),
       ...input,
-    }
+    };
 
     // TODO: Debemos insertar el job en la base de datos
-    return newJob
+    return newJob;
   }
 
   // Eliminar un job
   static async delete(id: string): Promise<boolean> {
     // TODO: Debemos eliminar el job de la base de datos
-    return false
+    return false;
   }
 
   // Actualizar un job
   static async update(id: string, input: UpdateJobDTO): Promise<Job | null> {
     // TODO: Debemos actualizar el job en la base de datos
-    return null
+    return null;
   }
 }
