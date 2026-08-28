3d48dabe437fc36f7e9b958956d5038e344277e4
madeval <alvarez.fing@gmail.com>
2026-08-28 16:11:22 -0300

    Refactor export of db in database.ts for clarity and remove redundancy

--- 11-ejercicio-sql/db/database.ts
+++ 11-ejercicio-sql/db/database.ts
@@ -1,8 +1,7 @@
 import Database from "better-sqlite3";
 
-const db = new Database("jobs.db");
+// Ojo con como exportabas el `db`. Era redundante, lo mejor es hacer un export de esta forma
+export const db = new Database("jobs.db");
 
 db.pragma("journal_mode = WAL"); // Mejora la concurrencia y performance
 db.pragma("foreign_keys = ON"); // sqlite por defecto ignora las claves foráneas, esto las habilita.
-
-export {db as export};
